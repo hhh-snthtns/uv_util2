28d7c7c34c55db60d94ceaf5c922850598e64d9a
aoyagikouhei <aoyagi.kouhei@gmail.com>
2014-11-05 15:00:35 +0900

    del

--- lib/uv_util2/db_util.rb
+++
@@ -1,9 +0,0 @@
-module UvUtil2
-  module DbUtil
-    def self.make_db(connect)
-      res = Sequel.connect(connect, after_connect: proc{|conn| conn.set_error_verbosity(PG::PQERRORS_VERBOSE)})
-      res.extension :pg_array, :pg_json
-      res
-    end
-  end
-end
