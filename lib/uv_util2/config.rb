752a89236a8e6999b26410b076999391042255b0
HOSHINO Hisato <hoshino_hisato@uniquevision.co.jp>
2023-10-06 13:14:31 +0900

    - refactoring.

--- lib/uv_util2/config.rb
+++ lib/uv_util2/config.rb
@@ -7,15 +7,20 @@ module UvUtil2
   class Config
     def self.read(path, env=nil)
       open(path) do |f|
-        yaml = nil
-        begin
-          yaml = ::YAML::load(::ERB.new(f.read).result, aliases: true).deep_symbolize_keys
-        rescue ArgumentError
-          yaml = ::YAML::load(::ERB.new(f.read).result).deep_symbolize_keys
-        end
+        yaml = parse(::ERB.new(f.read).result)
         env.present? ? yaml[env] : yaml
       end
     end
+
+    def self.parse(file_object)
+      begin
+        # for Ruby 3.x+
+        ::YAML::load(::ERB.new(f.read).result, aliases: true).deep_symbolize_keys
+      rescue ArgumentError
+        ::YAML::load(::ERB.new(f.read).result).deep_symbolize_keys
+      end
+    end
+    private_class_method :parse
   end
 end
 
