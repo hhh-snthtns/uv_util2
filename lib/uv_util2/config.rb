f66f094ed596055abfb13f4ce8ca23cd6754d711
HOSHINO Hisato <hoshino_hisato@uniquevision.co.jp>
2023-10-06 13:39:08 +0900

    - for running on Ruby3x+.

--- lib/uv_util2/config.rb
+++ lib/uv_util2/config.rb
@@ -7,10 +7,20 @@ module UvUtil2
   class Config
     def self.read(path, env=nil)
       open(path) do |f|
-        yaml = ::YAML::load(::ERB.new(f.read).result).deep_symbolize_keys
+        yaml = parse(f)
         env.present? ? yaml[env] : yaml
       end
     end
+
+    def self.parse(file_object)
+      begin
+        # for Ruby 3.x+
+        ::YAML::load(::ERB.new(file_object.read).result, aliases: true).deep_symbolize_keys
+      rescue ArgumentError
+        ::YAML::load(::ERB.new(file_object.read).result).deep_symbolize_keys
+      end
+    end
+    private_class_method :parse
   end
 end
 
